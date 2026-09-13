f55c5d60f437d8aabb47981a91c9c16e221cb151
djalberty-design <djalberty@gmail.com>
2026-09-12 21:40:42 -0400

    Delete .vercel/output directory

--- .vercel/output/functions/__server.func/_ssr/rank.worker-C91o6Hls.mjs
+++
@@ -1,6 +0,0 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/rank.worker-C91o6Hls.js
-function WorkerWrapper(options) {
-	return new Worker("/assets/rank.worker-Cnx8DdYw.js", { name: options?.name });
-}
-//#endregion
-export { WorkerWrapper as default };
--- .vercel/output/static/assets/rank.worker-CJXKFPuX.js
+++
@@ -1,1 +0,0 @@
-function e(e){return new Worker(`/assets/rank.worker-Cnx8DdYw.js`,{name:e?.name})}export{e as default};
